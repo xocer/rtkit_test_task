75ca0d4ddf0501195ce06d7758260e5851e63bbd
xocer <vitamin9292@mail.ru>
2021-11-09 18:06:11 +0300

    preparing of the project for show

--- db/schema.sql
+++ db/schema.sql
@@ -1,14 +1,16 @@
-create table rtkit_db
+create database rtkit_db;
+
+create table tsveta
 (
     id           serial primary key,
     color_number varchar(2),
     name         varchar(50)
 );
 
-insert into rtkit_db (color_number, name) VALUES ('01', 'Red');
-insert into rtkit_db (color_number, name) VALUES ('01', 'Orange');
-insert into rtkit_db (color_number, name) VALUES ('01', 'Yellow');
-insert into rtkit_db (color_number, name) VALUES ('01', 'Green');
-insert into rtkit_db (color_number, name) VALUES ('01', 'Blue');
-insert into rtkit_db (color_number, name) VALUES ('01', 'Dark blue');
-insert into rtkit_db (color_number, name) VALUES ('01', 'Violet');
+insert into tsveta (color_number, name) VALUES ('01', 'Red');
+insert into tsveta (color_number, name) VALUES ('02', 'Orange');
+insert into tsveta (color_number, name) VALUES ('03', 'Yellow');
+insert into tsveta (color_number, name) VALUES ('04', 'Green');
+insert into tsveta (color_number, name) VALUES ('05', 'Blue');
+insert into tsveta (color_number, name) VALUES ('06', 'Dark blue');
+insert into tsveta (color_number, name) VALUES ('07', 'Violet');
